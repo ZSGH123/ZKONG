dd02db893d1bb90765e4d4ad42114c8842148a1b
钟强 <@192.10.7.15>
2019-01-27 11:34:04 +0800

    首次提交

---
+++ Test/src/Hello/Hello.java
@@ -0,0 +1,13 @@
+package Hello;
+
+import java.util.List;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello");
+     
+	}
+
+}
