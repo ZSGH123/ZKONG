b09101477c5261c24f6e341baac1be1ebe690b08
钟强 <@192.10.7.47>
2019-02-14 11:55:19 +0800

    更新

--- Test/src/AgentDesignMode/Client.java
+++ Test/src/AgentDesignMode/Client.java
@@ -13,6 +13,14 @@ import java.util.List;
  */
 public class Client {
 
+	public void func(int i){
+		  while(i>0){
+		   i--;
+		   func(i);
+		   System.out.println("a"+i);
+	   }
+    }
+	
 	public static void main(String[] strs) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		//静态代理
 //		List listAgent=new Proxy(new ArrayList());
@@ -24,6 +32,16 @@ public class Client {
 	    List listAgent=(List) clz.getConstructor(new Class[]{InvocationHandler.class})//通过参数找到相应构造方法
 		.newInstance(new Object[]{new ListHandler(list)});//通过相应构造方法实例对象
 		listAgent.add("a");
+		int a=5,b=2;
+		float c=32.8f;
+		System.out.println(b*(int)c%a);
+		char d='男';
+		System.out.println(d);
 		
+		new Client().func(3);
+		
+		int n=11,i=4;
+		
+				System.out.println(n%=i+1);
 	}
 }
